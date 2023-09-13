a9ad8c9a3ca1418ecfe433d40ce37f93fb40aeba
IKaRos <w71800@gmail.com>
2023-09-13 12:12:17 +0800

    complete layout & RWD

--- static/scripts/attraction.js
+++ static/scripts/attraction.js
@@ -1,1 +1,21 @@
-console.log("Hello")
+const url = window.location.href;
+const id = url.match(/attraction\/(\d+)/)[1]
+
+function getData(){
+  return fetch(`/api/attraction/${id}`)
+  .then( res => res.json() )
+  .then( data => data.data )
+}
+
+// 將資料塞入頁面
+function render(){
+
+}
+
+async function init(){
+  let data = await getData()
+}
+
+
+
+
