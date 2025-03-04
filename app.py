e3fb73ed11af0e295f03cbc801c12c549bddc621
IKaRos <w71800@gmail.com>
2025-03-04 16:16:48 +0800

    chore: 調整運行時監聽的 port

--- app.py
+++ app.py
@@ -49,5 +49,5 @@ def dashboard():
 	return render_template("dashboard.html")
 
 
-port = int(os.environ.get("PORT", 8080))
+port = int(os.environ.get("PORT", 3000))
 app.run(host="0.0.0.0", port=port, debug=True)
