6338f5828853fbf8e6ae053d449ab4ebed5e2909
IKaRos <w71800@gmail.com>
2025-03-04 18:09:37 +0800

    chore: some test

--- app.py
+++ app.py
@@ -48,6 +48,6 @@ def thankyou():
 def dashboard():
 	return render_template("dashboard.html")
 
-
-port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port, debug=True)
+if __name__ == '__main__':
+	port = int(os.environ.get("PORT", 3000))
+	app.run(host="0.0.0.0", port=port, debug=True)
